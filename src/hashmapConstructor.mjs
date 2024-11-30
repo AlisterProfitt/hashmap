d485cd5dc436af6f5a8f10fcc9508127fb5b3a11
AlisterProfitt <alisterprofitt@gmail.com>
2024-11-30 13:26:58 -0400

    create set method

--- src/hashmapConstructor.mjs
+++ src/hashmapConstructor.mjs
@@ -19,11 +19,36 @@ class HashMap {
     }
 
     set(key, value) {
+        const bucketIndex = this.hash(key);
+        if (bucketIndex < 0 || bucketIndex >= this.buckets.length) {
+            throw new Error("Trying to access index out of bounds");
+        }
 
+        const newEntry = [key, value];
+        let potentialList = this.buckets[bucketIndex];
+        if (potentialList === undefined) {
+            const list = new LinkedList();
+            list.append(newEntry);
+            this.buckets[bucketIndex] = list;
+            return newEntry;
+        }
+
+        const listIndex = potentialList.find(key)
+        if (listIndex >= 0) {
+            potentialList.removeAt(listIndex);
+            potentialList.insertAt(newEntry, listIndex);
+        } else {
+            potentialList.append(newEntry);
+        }
+        return newEntry
     }
 }
 
 const test = new HashMap;
-console.log(test.buckets[3]);
+test.set('alister', 'biggus');
+test.set('joe', 'osteen')
+test.set('joel', 'osteen')
+test.set('alister', 'smallus')
+console.log(test.buckets[12]);
 
 export { HashMap };
